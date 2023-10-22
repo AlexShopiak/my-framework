b367bfe8aeadb03a4eb17a4c49cbf41a96f5512b
Alex Shopiak <alshop2004@gmail.com>
2023-10-22 02:43:58 +0000

    Start implementing Router

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const http = require('http')
+const http = require('http');
 const PORT = process.env.PORT || 5000;
+
+class Router {
+    constructor() {
+        this.endpoint = {};
+    }
+    request(method='GET', path, handler) {
+        if(!this.endpoint[path]) {
+            this.endpoint[path] = {};
+        }
+        const endpoint = this.endpoint[path];
+        if(endpoint[method]) {
+            throw new Error(`[${method}] on adress [${path}] already exists`);
+        }
+        endpoint[method] = handler;
+        //events
+    }
+}
 
 const server = http.createServer((req,res)=>{
     res.end('Hi')
 })
 
-server.listen(PORT, ()=>console.log('Started on:' + PORT))
+server.listen(PORT, () => console.log('Started on:' + PORT));
