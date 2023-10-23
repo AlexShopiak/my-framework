b20f9c0450dd6f1dd6f243318283588c12eadee2
Alex Shopiak <alshop2004@gmail.com>
2023-10-23 18:16:15 +0000

    Use middleware in index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Application = require('./framework/application.js');
-const userRouter = require('./src/user-router.js')
-const PORT = process.env.PORT || 5000;
+const userRouter = require('./src/user-router.js');
+const JSONParser = require('./framework/parseJSON.js');
 
+const PORT = process.env.PORT || 5000;
 const app = new Application();
 
+app.use(JSONParser);
 app.addRouter(userRouter);
 app.listen(PORT, () => console.log('Started on:' + PORT))
