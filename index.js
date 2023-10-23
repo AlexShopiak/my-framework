c158ea37c8b57cd7ec09cfcd7b2b2b274fd4c385
Alex Shopiak <alshop2004@gmail.com>
2023-10-23 12:07:56 +0000

    Create Application wrapper

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const EventEmitter = require('events');
+//const http = require('http');
+//const EventEmitter = require('events');
 const Router = require('./framework/router.js');
 
 const PORT = process.env.PORT || 5000;
-const emitter = new EventEmitter()
+//const emitter = new EventEmitter()
 const router = new Router();
 
 router.get('/users', (req, res) => {
@@ -14,11 +14,6 @@ router.get('/posts', (req, res) => {
     res.end('Hi from posts');
 })
 
-const server = http.createServer((req,res)=>{
-    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
-    if (!emitted) {
-        res.end();
-    }
-})
+//const server = http.createServer()
 
 server.listen(PORT, () => console.log('Started on:' + PORT));
