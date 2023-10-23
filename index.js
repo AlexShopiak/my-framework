0c219acc63c48ccee8028d9cefc6d5cc63bef7d4
Alex Shopiak <alshop2004@gmail.com>
2023-10-23 15:53:19 +0000

    Use new router in index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const Application = require('./framework/application.js');
-const Router = require('./framework/router.js');
-
+const userRouter = require('./src/user-router.js')
 const PORT = process.env.PORT || 5000;
 
 const app = new Application();
-const router = new Router();
 
-router.get('/users', (req, res) => {
-    res.end('Hi from users');
-})
-
-router.get('/posts', (req, res) => {
-    res.end('Hi from posts');
-})
-
-app.addRouter(router);
+app.addRouter(userRouter);
 app.listen(PORT, () => console.log('Started on:' + PORT))
