8a013c5ca84e19c60356ebc75b72f15414fdacbe
Alex Shopiak <alshop2004@gmail.com>
2023-10-25 15:24:02 +0000

    Connect to Mongo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Application = require('./framework/application.js');
 const userRouter = require('./src/user-router.js');
 const JSONParser = require('./framework/parseJSON.js');
 const URLParser = require('./framework/parseURL.js');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 const app = new Application();
@@ -9,4 +10,14 @@ const app = new Application();
 app.use(JSONParser);
 app.use(URLParser('http://localhost:5000'));
 app.addRouter(userRouter);
-app.listen(PORT, () => console.log('Started on:' + PORT))
+
+const start = async() => {
+    try {
+        await mongoose.connect('mongodb+srv://alex:0000@cluster0.u0awupe.mongodb.net/?retryWrites=true&w=majority');
+        app.listen(PORT, () => console.log('Started on:' + PORT));
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+start();
