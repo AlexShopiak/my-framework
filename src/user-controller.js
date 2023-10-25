d804fbe4e1e19987055867ad19d9616e65c18212
Alex Shopiak <alshop2004@gmail.com>
2023-10-25 16:31:48 +0000

    Implement POST method and improve other

--- src/user-controller.js
+++ src/user-controller.js
@@ -1,49 +1,76 @@
+//TODO: res.send should only accept one type of data!!
+
 const User = require('./user-model.js');
 
 const getUsers = async (req, res) => {
-    let users = [];
+    let result;
     if (req.params.name) {
-        users = await User.find({name:req.params.name});
+        const users = await User.find({name:req.params.name});
+        if (users.length == 0) {
+            result = "User does not exist";
+        } else {
+            result = users;
+        }
     } else {
-        users = await User.find();
+        result = await User.find();
     }
-
-    if (users.length == 0) {
-        res.send("User does not exist");
-    } else {
-        res.send(users);
-    }
+    res.send(result);
 }
 
 const createUsers = async (req, res) => {
-    const exist = await User.find({name:req.body.name});
-    console.log(exist)
-    if (exist.length == 0) {
-        const user = await User.create(req.body);
-        res.send(user);
+    let result;
+    const users = await User.find({name:req.body.name});
+
+    if (users.length == 0) {
+        result = await User.create(req.body);
     } else {
-        res.send("User with this name already exist");
+        result = "User with this name already exists";
     }
-    
+    res.send(result);
+}
+
+const putUser = async (req, res) => {
+    let result;
+    if (req.params.name) {
+        const users = await User.find({name:req.params.name});
+
+        if (users.length !== 0) {
+            await User.updateOne({name:req.params.name}, req.body);
+            result = `${req.params.name} was updated`;
+        } else {
+            if (req.params.name == req.body.name) {
+                result = await User.create(req.body);
+            } else {
+                result = "Invalid name in req.body";
+            }
+        }
+    } else {
+        result = "Sorry, but you have to pass a user name to update it";
+    }
+    res.send(result);
 }
 
 const deleteUsers = async (req, res) => {
-    let users = [];
+    let result;
     if (req.params.name) {
-        users = await User.find({name:req.params.name});
-    }
+        const users = await User.find({name:req.params.name});
 
-    if (users.length !== 0) {
-        await User.deleteOne({name:req.params.name})
-        res.send(`${req.params.name} was deleted`);
+        if (users.length !== 0) {
+            await User.deleteOne({name:req.params.name})
+            result = `${req.params.name} was deleted`;
+        } else {
+            result = "User with this name already exist";
+        }
     } else {
         await User.deleteMany({});
-        res.send("All users were deleted");
+        result = "All users were deleted";
     } 
+    res.send(result);
 }
 
 module.exports = {
     getUsers,
     createUsers,
+    putUser,
     deleteUsers,
 }
--- src/user-router.js
+++ src/user-router.js
@@ -4,6 +4,7 @@ const router = new Router();
 
 router.get('/users', controller.getUsers);
 router.post('/users', controller.createUsers);
+router.put('/users', controller.putUser);
 router.delete('/users', controller.deleteUsers);
 
 module.exports = router;
