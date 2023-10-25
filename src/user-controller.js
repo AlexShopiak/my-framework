db3d744080d4b1e5e0ecf57212f4db8879101019
Alex Shopiak <alshop2004@gmail.com>
2023-10-25 15:51:04 +0000

    Implement unique names system

--- src/user-controller.js
+++ src/user-controller.js
@@ -13,12 +13,18 @@ const getUsers = async (req, res) => {
     } else {
         res.send(users);
     }
-    
 }
 
 const createUsers = async (req, res) => {
-    const user = await User.create(req.body);
-    res.send(user);
+    const exist = await User.find({name:req.body.name});
+    console.log(exist)
+    if (exist.length == 0) {
+        const user = await User.create(req.body);
+        res.send(user);
+    } else {
+        res.send("User with this name already exist");
+    }
+    
 }
 
 const deleteUsers = async (req, res) => {
